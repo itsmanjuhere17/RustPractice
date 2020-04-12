55a57e985a52255df9e2046a5b7feeb3db92b98a
manju malepati <manjumalepati17@gmail.com>
2020-04-12 09:06:48 +0300

    Learning:Closure Adaptors and Iterators

--- src/functionalprogramming.rs
+++ src/functionalprogramming.rs
@@ -28,6 +28,12 @@ impl<T> Cacher<T>
     }
 }
 
+#[derive(Debug,PartialEq)]
+struct Shoe{
+    size:u32,
+    model:String
+}
+
 pub fn functionprog(){
     println!("########### Inside Function Programming #######################");
     println!("Dummy value using closure is:{}",dummy());
@@ -39,13 +45,34 @@ pub fn functionprog(){
     //iterators.
     let eleVec = vec![2,3,4,1,5];
     let mut iter_ = eleVec.iter();
-    for ele in iter_.next(){
+    for ele in iter_{
         println!("vector ele is:{}",ele);
     }
     //Methods that consume iterators..
-    let resSum:u32 = iter_.sum(); //Here, sum() consumes the iterator by calling next internally
+    let eleVec = vec![1,3,6,8];
+    let mut newiter = eleVec.iter();
+    let resSum:u32 = newiter.sum(); //Here, sum() consumes the iterator by calling next internally
     println!("Sum of vector is:{}",resSum);
     //println!("Using iter_ after sum:{:?}",iter_);
+
+    //Methods that produce other iterators.
+    let list = vec![10,30,20,15];
+    let newList:Vec<_> = list.iter().map(|x|{
+        x*2
+    }).collect();
+    for ele in newList{
+        println!("New list is:{}",ele);
+    }
+
+    //  Closures that capture environment.
+    let mut shoes = Vec::new();
+    shoes.push(Shoe{size:10,model:"sneaker".to_string()});
+    shoes.push(Shoe{size:12,model:"flats".to_string()});
+    shoes.push(Shoe{size:10,model:"heels".to_string()});
+    let filter_shoe = filter_on_shoe_size(shoes,10);
+    for shoe in filter_shoe{
+        println!("Shoe is:{:?}",shoe);
+    }
     println!("############### Exiting Function Programming ################");
 }
 
@@ -54,4 +81,8 @@ pub fn dummy()->u32{
        arg
     }); //Declaring a closure with Cacher type.
     closure.getvalue(10) //Calling with a closure.
+}
+
+fn filter_on_shoe_size(shoes:Vec<Shoe>,size:u32)->Vec<Shoe>{
+    shoes.into_iter().filter(|s| s.size == size).collect()
 }
